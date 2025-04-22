681bcac3e6d55dbb77c0c574d3dd891555333951
Gunnar <gunnar@nemont.net>
2025-04-22 13:17:14 -0600

    LCD functionality, no i2c because I'm out of time :(

--- i2c-LCD/main.c
+++ i2c-LCD/main.c
@@ -16,40 +16,22 @@ int main(void)
     while (true) {
 
         start_up();
-        start_up();
 
         while (true) {
-            pattern_name(0);
-            display_window_size('9');
-            display_temp(30.4);
-            pattern_name(1);
-            display_window_size('8');
-            display_temp(20.1);
-            pattern_name(2);
-            display_window_size('7');
-            display_temp(24.6);
-            pattern_name(3);
-            display_window_size('6');
-            display_temp(25.6);
-            pattern_name(4);
-            display_window_size('5');
-            display_temp(35.3);
-            pattern_name(5);
-            display_window_size('4');
-            display_temp(19.9);
-            pattern_name(6);
-            display_window_size('3');
-            display_temp(99.9);
-            pattern_name(7);
-            display_window_size('2');
-            display_temp(11.1);
-            pattern_name(8);
-            display_window_size('1');
-            display_temp(34.2);
-            pattern_name(9);
-            display_window_size('0');
-            display_temp(56.1);
-            pattern_name(10);
+            display_mode_string(0);
+            display_window_size(3);
+            display_ambient_temperature('2', '3', '4');
+            display_plant_temperature('1', '5', '3');
+            display_time_in_state(96);
+            display_mode_string(1);
+            display_window_size(4);
+            display_time_in_state(124);
+            display_mode_string(2);
+            display_ambient_temperature('4', '3', '2');
+            display_plant_temperature('1', '0', '9');
+            display_mode_string(3);
+            display_time_in_state(5);
+            display_mode_string(4);
         }
     }
 }
